b34f9039dbe583c7b72d5f8a89e985de2a2df9fc
Ian McLerran <imclerran@gmail.com>
2019-04-03 00:39:13 -0700

    updated task.js to use correct date format, added completed and subtask fields

--- js/task.js
+++ js/task.js
@@ -3,8 +3,10 @@ export class Task {
         this.id = -1;
         this.title = "";
         this.priority = 0;
-        this.due = "1/1/1970";
+        this.due = "01-01-1970T00:00:00.000+00:00";
+        this.completed = null;
         this.category = "none";
         this.timeblock = { start: null, end: null, };
+        this.subtasks = [];
     }
 }

e9a7580d3b00bcaa53e5bf9772868314d50aa817
Isaac <isaac.schultz@wsu.edu>
2019-04-10 15:44:54 -0700

    Implementing add task function

--- RESTful API/models/TaskModel.js
+++ RESTful API/models/TaskModel.js
@@ -15,21 +15,71 @@ var User = function (task) {
     this.TimeBlocks = [];
 }
 
-Users.addTask = function (Tasks, taskId, result) {
+Tasks.addTask = function (Tasks, taskName, result) {
+    var resultObj, testTask, regex; // variables used throughout the function.
+    resultObj = { //what will be returned to the requester when the function completes
+        taskId: "",
+        success: false, //task is not added by default
+        statusMsg: "",
+        statusObj: null
+    };
 
+    // TODO: Probably don't need this but Ill leave it here for now
+    regex = /^(?:[ '.\-a-zA-Z]*)?$/gm; //Regex tests for only alphabetics, periods, spaces, and hyphens
+
+    if (!(typeof taskName == 'string') || taskName.length == 0 || !(regex.test(taskName))) { //invalid taskName. 
+        resultObj.statusMsg = "Error: Task was not added because taskName is invalid. (is not a string, is 0 characters, or does not match Regex)";
+        result(resultObj, null);
+    } else { //data is valid
+        testTask = { //create a task to query the database with
+            taskName: taskName
+        };
+
+        //Query the database for any Tasks with taskName
+        Tasks.find(testTask).toArray(function (err, res) {
+            if (err) { //Unkown error, return to client and display it in the log.
+                resultObj.statusMsg = "There was an error when testing if the task already exists in the database";
+                console.log(resultObj.statusMsg);
+                resultObj.statusObj = err;
+                result(resultObj, null)
+            } else if (res.length > 0) { //Task is already in database
+                resultObj.statusMsg = "Task already exists in the database";
+                console.log(res[0].taskId);
+                result(resultObj, null);
+            } else { //Task is not in database, so we need to add it
+
+                //build out the testTask to be used to insert into the databse
+                testTask.taskId = taskName + Math.random().toString(36).substr(2); //random string after taskName as the key\
+                testTask = new Task(testTask); //build the rest of the parameters for a Task by using the constructor
+
+                //Add the new Task into the database
+                Tasks.insertOne(testTask, function (err2) {
+                    if (err2) { //Unkown error, return to client and display it in the log.
+                        resultObj.statusMsg = "Error when adding Task to database";
+                        console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
+                        resultObj.statusObj = err2;
+                        result(resultObj, null);
+                    } else { //Task was added to the database.
+                        resultObj.taskId = testTask.taskId; //return taskId and that they were added.
+                        resultObj.success = true;
+                        result(resultObj, null);
+                    }
+                });
+            }
+        });
+    }
+}
+
+
+Tasks.getTask = function (Tasks, taskId, result) {
+
+}
+
+Tasks.updateTask = function (Tasks, taskId, result) {
 
 }
 
 
-Users.getTask = function (Tasks, taskId, result) {
-
-}
-
-Users.updateTask = function (Tasks, taskId, result) {
-
-}
-
-
-Users.deleteTask = function (Tasks, taskId, result) {
+Tasks.deleteTask = function (Tasks, taskId, result) {
 
 }

e483cacf7bbc87c5d526a7102bd0e2147005befb
Isaac <isaac.schultz@wsu.edu>
2019-04-11 11:31:16 -0700

    Added controller definitinons for C and R in CRUD

--- RESTful API/controllers/TaskController.js
+++ RESTful API/controllers/TaskController.js
@@ -1,24 +1,35 @@
 /* CptS 489, Spring 2019
     Project: Task Tracker
-    File: UserController.js    */
+    File: TaskController.js    */
 
     'use strict'
 
     const Task = require('../models/TaskModel.js');
     
-    exports.addTask = function (res, err) {
-      var newTask = new Tass(req.body);
-      Users.addTask(req.app.locals.Tasks, newTask, function (err, result) {
+    exports.addTask = function (req, res) {
+      var newTask = new Task(req.body);
+      Task.addTask(req.app.locals.tasks, newTask, function (err, result) {
         if (err) {
           res.send(err);
         } else {
           res.json(result);
         }
-      })
+      });
     };
     
-    exports.getTask = function (res, err) {
-      Users.getUsers(req.app.locals.Tasks, function (err, result) {
+    //we need to deal with determining if the request is for a single task or for all the tasks from a certain user
+    exports.getTask = function (req, res) {
+      Task.getTask(req.app.locals.tasks, req.params.id, function (err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(result);
+        }
+      });
+    }
+
+    exports.getTasks = function (req, res) {
+      Task.getTasks(req.app.locals.tasks, req.params.id, function (err, result) {
         if (err) {
           res.send(err);
         } else {
@@ -27,10 +38,10 @@
       });
     }
     
-    exports.updateUser = function (res, err) {
+    exports.updateTask = function (req, res) {
     
     }
     
-    exports.deleteUser = function (res, err) {
+    exports.deleteTask = function (req, res) {
 
     }

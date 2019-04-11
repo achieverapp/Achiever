7789d19412f1518a6cea41bfc642c35d4f631d13
Isaac <isaac.schultz@wsu.edu>
2019-04-10 19:52:00 -0700

    Fixed Formatting

--- RESTful API/controllers/UserController.js
+++ RESTful API/controllers/UserController.js
@@ -1,62 +1,42 @@
 /* CptS 489, Spring 2019
     Project: Task Tracker
-    File: UserController.js    */ 
+    File: UserController.js    */
 
-    'use strict'
+'use strict'
 
-    const Users=require('../models/UserModel.js');
+const Users = require('../models/UserModel.js');
 
-    exports.addUser= function(res,err)
-    {
-        var newUser=new Users(req.body);
-        Users.addUser(req.app.locals.Users,newplayer,function(err,result) {
-
-            if (err) {
-     
-                res.send(err);
-     
-            } else {
-     
-                res.json(result);
-     
-            }
-            })
-        };
-        
-    
-    exports.getUser=function(res,err)
-    {
-        Users.getUsers(req.app.locals.Users,function(err,result)
-        {
-          if (err) {
-           
-         
-            res.send(err);
-      
-          } else {
-      
-            res.json(result);
-      
-          }
-        });
+exports.addUser = function (res, err) {
+  var newUser = new Users(req.body);
+  Users.addUser(req.app.locals.Users, newUser, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
     }
-    exports.updateUser=function(res,err)
-    {
+  })
+};
 
+exports.getUser = function (res, err) {
+  Users.getUsers(req.app.locals.Users, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
     }
-    exports.deleteUser=function(res,err)
-    {
-        Users.deleteUsers(req.app.locals.Users,function(err,result)
-        {
-          if (err) {
-           
-         
-            res.send(err);
-      
-          } else {
-      
-            res.json(result);
-      
-          }
-        });
+  });
+}
+
+exports.updateUser = function (res, err) {
+
+}
+
+exports.deleteUser = function (res, err) {
+  Users.deleteUsers(req.app.locals.Users, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
     }
+  });
+}
--- RESTful API/models/UserModel.js
+++ RESTful API/models/UserModel.js
@@ -6,26 +6,24 @@
 //var noSql = require('./noSqlDb.js');
 var ObjectId = require('mongodb').ObjectId;
 
-var User=function(User)
-{
+var User = function (User) {
   this.name = User.name;
-  this.userName=User.userName;
-  this._id=User._id;
+  this.userName = User.userName;
+  this._id = User._id;
   this.savedTasks = [];
   this.savedTasks = user.savedTasks;
 }
 
 
 // Takes a new User object that contains all data that we want to add.
-Users.addUser=function(User,newUser,result)
-{
-    var resultObj, testUser, regex; // variables used throughout the function.
+Users.addUser = function (User, newUser, result) {
+  var resultObj, testUser, regex; // variables used throughout the function.
   resultObj = { //what will be returned to the requester when the function completes
     userId: "",
     success: false, //user is not added by default
     statusMsg: "",
     statusObj: null
-  };  
+  };
 
   if (!(typeof newUser.userName == 'string') || newUser.userName.length == 0) { //invalid username. 
     resultObj.statusMsg = "Error: user was not added because username is invalid. (is not a string, is length 0, or does not match Regex)";
@@ -75,51 +73,50 @@ Users.addUser=function(User,newUser,result)
 GetUser returns the data of the user with the given ID.
 If no user is found, there is no data retuned and a statusMsg with the reason why there was an error.
 */
-Users.getUser=function(Users,UserId,result)
-{
-var resultObj={ success:false,statusMsg:"",statusObj:null};
-Users.find({_id: UserId}).toArray(function(err,res)
-{
-    if(err)
-    {
-        resultObj.statusMsg="There was an error when trying to access user in database.";
-        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-        resultObj.statusObj = err;
-        result(resultObj, null);
-    }
-    else if(res.length==1)
-    {
-    resultObj.statusMsg = "User retrieved.";
-      resultObj.success = true;
-      resultObj.data = res[0];
-      result(resultObj, null);
-    }
-    else{
-    resultObj.statusMsg = "user not found";
-    result(resultObj, null);
-    }
-})
-}
-
-Users.updateUser=function(users,user,result)
-{  
-
-  //Three different Cases:
-  //  User adds a new task to save
-  //  User updates their name
-  //  User updates thier other information (Not implemented yet)
-
-  newUser = new User(user);
+Users.getUser = function (Users, UserId, result) {
   var resultObj = {
     success: false,
     statusMsg: "",
     statusObj: null
   };
-
-  //Check if the user is in the database
-  users.find({
-    _id: user._id
+  Users.find({
+    _id: UserId
   }).toArray(function (err, res) {
+    if (err) {
+      resultObj.statusMsg = "There was an error when trying to access user in database.";
+      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+      resultObj.statusObj = err;
+      result(resultObj, null);
+    } else if (res.length == 1) {
+      resultObj.statusMsg = "User retrieved.";
+      resultObj.success = true;
+      resultObj.data = res[0];
+      result(resultObj, null);
+    } else {
+      resultObj.statusMsg = "user not found";
+      result(resultObj, null);
+    }
+  })
+}
+
+Users.updateUser = function (users, user, result) {
+
+//Three different Cases:
+//  User adds a new task to save
+//  User updates their name
+//  User updates thier other information (Not implemented yet)
+
+newUser = new User(user);
+var resultObj = {
+  success: false,
+  statusMsg: "",
+  statusObj: null
+};
+
+//Check if the user is in the database
+users.find({
+  _id: user._id
+}).toArray(function (err, res) {
     if (err) { //Unkown error, return to client and display it in the log.
       resultObj.statusMsg = "Error when checking if user with id " + userId + " exists in database.";
       resultObj.statusObj = err;
@@ -150,154 +147,150 @@ Users.updateUser=function(users,user,result)
             }
           })
           .toArray(function (err2, res2) {
-            if (err2) { //Unkown error, return to client and display it in the log.
-              resultObj.statusMsg = "Error when locating user";
-              console.log(resultObj.statusMsg + ": " + err2);
-              resultObj.statusObj = err2;
-              result(resultObj, null);
-            }
-                  
-                //Insert a new hole if it is not already in the data
-                if (res2.length == 0) {                
-                  //insert updated totals and holedata into the database
-                  users.updateOne({ //select the user with the given userId to update
-                      _id: user._id
-                    }, {
-                      $push: { //adds the holedata as a new element of the holes array
-                        holes: {
-                          holeNum: holeData.holeNum,
-                          str: holeData.str,
-                          time: holeData.time
+              if (err2) { //Unkown error, return to client and display it in the log.
+                resultObj.statusMsg = "Error when locating user";
+                console.log(resultObj.statusMsg + ": " + err2);
+                resultObj.statusObj = err2;
+                result(resultObj, null);
+              }
+
+              //Insert a new hole if it is not already in the data
+              if (res2.length == 0) {
+                //insert updated totals and holedata into the database
+                users.updateOne({ //select the user with the given userId to update
+                    _id: user._id
+                  }, {
+                    $push: { //adds the holedata as a new element of the holes array
+                      holes: {
+                        holeNum: holeData.holeNum,
+                        str: holeData.str,
+                        time: holeData.time
+                      }
+                    },
+                  },
+                  function (err4, res4) {
+                    if (err4) { //Unkown error, return to client and display it in the log.
+                      resultObj.statusMsg = "Error when attempting to add new hole " + holeData.holeNum + " for user " + user.userName;
+                      console.log(resultObj.statusMsg + ": " + err4);
+                      resultObj.statusObj = err4;
+                      resultObj.total = null;
+                      result(resultObj, null);
+                    } else { //Hole was added successfully!
+                      resultObj.statusMsg = "Hole " + holeData.holeNum + " successfully added for user " + user.userName;
+                      resultObj.success = true;
+                      resultObj.total = totalObject;
+                      result(resultObj, null);
+                    }
+                  });
+              } else { // That user has already played that hole, so we need to update their old data
+
+                //calculate pars for the new data
+                for (var i = 0; i < user.holes.length; i++) { //adds up all the par hole data for the number of holes the user is on
+                  var holeTimePar = new SGS(pars.holes[i].time); //gets the par time for the hole
+                  sgsPar = sgsPar.addToMany([holeTimePar, new SGS(pars.holes[i].str, 0)]); //adds up all the times and strokes for the holes to build the SGS par
+                  timePar = timePar.addTo(holeTimePar); //adds up all the times for the total par Time
+                  strokesPar += pars.holes[i].str; //adds up all the strokes for the total par strokes
+                }
+
+                // First we need to calcuate what the updated totals will be             
+                var oldHoleData = user.holes[holeData.holeNum - 1]; //gets the old hole data
+
+                // Only need to update if there actually was a change in the data
+                if (holeData.str != oldHoleData.str || holeData.time != oldHoleData.time) {
+                  var totalObject = {}, // initialize some variables that we need
+                    update = {};
+
+                  // Update that user in the database
+                  users.updateOne({ //find the user to update
+                      userId: userId,
+                      holes: {
+                        $elemMatch: {
+                          holeNum: holeData.holeNum
                         }
-                      },
+                      }
+                    }, { //update its data with:
+                      $set: {
+                        'holes.$.holeNum': holeData.holeNum,
+                        'holes.$.str': holeData.str,
+                        'holes.$.time': holeData.time,
+
+                        'total.sgs': totalObject.sgs,
+                        'total.sgsToPar': totalObject.sgsToPar,
+                        'total.str': totalObject.str,
+                        'total.strToPar': totalObject.strToPar,
+                        'total.time': totalObject.time,
+                        'total.timeToPar': totalObject.timeToPar
+                      }
                     },
                     function (err4, res4) {
                       if (err4) { //Unkown error, return to client and display it in the log.
-                        resultObj.statusMsg = "Error when attempting to add new hole " + holeData.holeNum + " for user " + user.userName;
+                        resultObj.statusMsg = "Error when attempting to update Hole " + holeData.holeNum + " for user " + user.userName;
                         console.log(resultObj.statusMsg + ": " + err4);
                         resultObj.statusObj = err4;
                         resultObj.total = null;
                         result(resultObj, null);
-                      } else { //Hole was added successfully!
+                      } else { //hole updated successfully!
                         resultObj.statusMsg = "Hole " + holeData.holeNum + " successfully added for user " + user.userName;
                         resultObj.success = true;
                         resultObj.total = totalObject;
                         result(resultObj, null);
                       }
                     });
-                } else { // That user has already played that hole, so we need to update their old data
-
-                  //calculate pars for the new data
-                  for (var i = 0; i < user.holes.length; i++) { //adds up all the par hole data for the number of holes the user is on
-                    var holeTimePar = new SGS(pars.holes[i].time); //gets the par time for the hole
-                    sgsPar = sgsPar.addToMany([holeTimePar, new SGS(pars.holes[i].str, 0)]); //adds up all the times and strokes for the holes to build the SGS par
-                    timePar = timePar.addTo(holeTimePar); //adds up all the times for the total par Time
-                    strokesPar += pars.holes[i].str; //adds up all the strokes for the total par strokes
-                  }
-
-                  // First we need to calcuate what the updated totals will be             
-                  var oldHoleData = user.holes[holeData.holeNum - 1]; //gets the old hole data
-
-                  // Only need to update if there actually was a change in the data
-                  if (holeData.str != oldHoleData.str || holeData.time != oldHoleData.time) {
-                    var totalObject = {}, // initialize some variables that we need
-                      update = {};
-
-                    // Update that user in the database
-                    users.updateOne({ //find the user to update
-                        userId: userId,
-                        holes: {
-                          $elemMatch: {
-                            holeNum: holeData.holeNum
-                          }
-                        }
-                      }, { //update its data with:
-                        $set: {
-                          'holes.$.holeNum': holeData.holeNum,
-                          'holes.$.str': holeData.str,
-                          'holes.$.time': holeData.time,
-
-                          'total.sgs': totalObject.sgs,
-                          'total.sgsToPar': totalObject.sgsToPar,
-                          'total.str': totalObject.str,
-                          'total.strToPar': totalObject.strToPar,
-                          'total.time': totalObject.time,
-                          'total.timeToPar': totalObject.timeToPar
-                        }
-                      },
-                      function (err4, res4) {
-                        if (err4) { //Unkown error, return to client and display it in the log.
-                          resultObj.statusMsg = "Error when attempting to update Hole " + holeData.holeNum + " for user " + user.userName;
-                          console.log(resultObj.statusMsg + ": " + err4);
-                          resultObj.statusObj = err4;
-                          resultObj.total = null;
-                          result(resultObj, null);
-                        } else { //hole updated successfully!
-                          resultObj.statusMsg = "Hole " + holeData.holeNum + " successfully added for user " + user.userName;
-                          resultObj.success = true;
-                          resultObj.total = totalObject;
-                          result(resultObj, null);
-                        }
-                      });
-                  } else { // New hole data is the same as what is in the database, so we didnt need to update anything!
-                    resultObj.statusMsg = "Hole Data unchanged, update contained the same data already stored in database";
-                    result(resultObj, null);
-                  }
+                } else { // New hole data is the same as what is in the database, so we didnt need to update anything!
+                  resultObj.statusMsg = "Hole Data unchanged, update contained the same data already stored in database";
+                  result(resultObj, null);
                 }
               }
-            });
+            }
           });
-      }
-    }
-  });
+    });
+}
+}
+});
 }
 
 /*delete a user from the database
 searching for the userId(for now we will find by userId) and remove them from the database
 
 */
-Users.deleteUser=function(Users,UserId,result)
-{
-var resultObj={success:false,statusMsg:"", statusMsg:null}
-users.find(_id= userId).toArray(function(err,res)
-{
+Users.deleteUser = function (Users, UserId, result) {
+  var resultObj = {
+    success: false,
+    statusMsg: "",
+    statusMsg: null
+  }
+  users.find(_id = userId).toArray(function (err, res) {
     //if err
-    if(err)
-    {
-        resultObj.statusMsg="There was an error when trying to access user in database.";
-        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-        resultObj.statusObj = err;
-        result(resultObj, null);
+    if (err) {
+      resultObj.statusMsg = "There was an error when trying to access user in database.";
+      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+      resultObj.statusObj = err;
+      result(resultObj, null);
     }
     //if one
-    else if(res==1)
-    {
-        Users.deleteOne({_id:UserId},function(err2,res2)
-        {
+    else if (res == 1) {
+      Users.deleteOne({
+        _id: UserId
+      }, function (err2, res2) {
         //if err
-           if(err)
-           {
-            resultObj.statusMsg="unable to delete the specified userId";
-            console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
-            resultObj.statusObj = err;
-            result(resultObj, null);
-           }
-           //delete
-           else
-           {
-               resultObj.success=true;
-               resultObj.statusMsg="user with"+userId+"was deleted";
-               result(resultObj,null);
-           }
-        })  
+        if (err) {
+          resultObj.statusMsg = "unable to delete the specified userId";
+          console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
+          resultObj.statusObj = err;
+          result(resultObj, null);
+        }
+        //delete
+        else {
+          resultObj.success = true;
+          resultObj.statusMsg = "user with" + userId + "was deleted";
+          result(resultObj, null);
+        }
+      })
     }
     //if nothing
-    else
-    {
-    resultObj.statusMsg="user with"+userId+"was not found";
-    result(resultObj, null);
+    else {
+      resultObj.statusMsg = "user with" + userId + "was not found";
+      result(resultObj, null);
     }
-});
+  });
 }
-
-

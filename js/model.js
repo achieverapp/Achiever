55dc000c8e0ad29bbc191f72882f9289626cb588
Isaac <isaac.schultz@wsu.edu>
2019-04-15 11:30:56 -0700

    Added function to retrieve parameters from URL

--- js/model.js
+++ js/model.js
@@ -48,3 +48,19 @@ function updateTask(task) {
         tasks[i] = task;
     }
 }
+
+//https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};

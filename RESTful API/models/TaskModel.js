73c9e31a258e338432df2fe305900ed88646a0d4
Isaac <Isaac.Schultz@wsu.edu>
2019-04-16 22:52:26 -0700

    Changed into Classes

--- RESTful API/models/TaskModel.js
+++ RESTful API/models/TaskModel.js
@@ -5,7 +5,155 @@
 'user strict';
 
 var ObjectId = require('mongodb').ObjectId;
-var Task = require('../../Constructors.js').Task;
+var SubTask = require('./SubTask.js').default;
+
+class Task {
+  constructor(task) {
+    this.owner = task.owner == null ? null : task.owner;
+    this.title = task.title == null ? null : task.title;
+    this.category = task.category == null ? none : task.category;;
+    this.priority = task.priority == null ? 0 : task.priority;
+    this.subTasks = task.subTasks == null ? [] : task.subTasks;
+    this.timeBlocks = task.timeBlocks == null ? [] : task.timeBlocks;
+    this.due = task.due == null ? new Date() : task.due;
+  }
+  /*
+    add a task to the task database
+    if the task can't be added then just return an err message
+  */
+  static addTask(tasksDB, taskName, result) {
+    var resultObj;
+    tasksDB.insertOne(taskName, function (err, res) {
+      if (err) { //Unkown error, return to client and display it in the log.
+        resultObj = ResultObj("Error when adding new Task to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else {
+        resultObj = ResultObj("Added task " + taskName.title, null, true, taskName._id, taskName);
+        result(resultObj);
+      }
+    });
+  }
+  /*
+    get a single task
+    if the task is not available then the return an err message,else return the task informationto
+  */
+  static getTask(tasksDB, taskId, result) {
+    var resultObj, id;
+    if (taskId == "default") { // If the id is 'default' then we cannot create an ObjectId with it and mus tjust pass it as a string.
+      id = taskId;
+    } else {
+      id = new ObjectId(taskId);
+    }
+    tasksDB.find({
+      _id: id
+    }).toArray(function (err, res) {
+      if (err) {
+        resultObj = ResultObj("Error when adding task to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else if (res.length == 1) {
+        resultObj = ResultObj("Task retrieved", null, true, res[0]._id, res[0]);
+        result(resultObj);
+      } else {
+        resultObj = ResultObj("Task not found");
+        result(resultObj);
+      }
+    });
+  }
+  //get all tasks for a specific user
+  static getTasks(tasksDB, userId, result) {
+    var resultObj;
+    tasksDB.find({
+      owner: userId
+    }).toArray(function (err, res) {
+      if (err) {
+        resultObj = ResultObj("Error when adding task to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else {
+        resultObj = ResultObj("Task retrieved", null, true, userId, res);
+        result(resultObj);
+      }
+    });
+  }
+  // TODO: Implement U and D in milestone 3
+  /*update the task for several cases
+  -name
+  -category
+  -priority
+  -subtask
+  -timeblock?*/
+  static updateTask(tasksDB, newTask, result) {
+    var resultObj;
+    tasksDB.find({
+      _id: new ObjectId(newTask._id)
+    }).toArray(function (err, res) {
+      if (err) { //Unkown error, return to client and display it in the log.
+        resultObj = ResultObj("Error when checking if user with id " + taskId + " exists in database.", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else if (res.length == 0) { //no user with id userId, tell the updater and log it
+        resultObj = ResultObj("Task not in database. ID:" + taskId._Id);
+        console.log(resultObj.statusMsg);
+        result(resultObj);
+      } else {
+        if (newTask.name != null) {
+          updateTaskName(tasksDB, newTask, result).then(function (res) {
+            result(res);
+          });
+        }
+        if (newTask.category != null) {
+          updateTaskCategory(tasksDB, newTask, result).then(function (res) {
+            result(res);
+          });
+        }
+        if (newTask.priority != null) {
+          updateTaskPriority(tasksDB, newTask, result).then(function (res) {
+            result(res);
+          });
+        }
+        if (newTask.timeBlocks.length > 0) {
+          updateTaskTB(tasksDB, newTask, result).then(function (res) {
+            result(res);
+          });
+        }
+        if (newTask.subTasks.length > 0) {
+          updatesubTask(tasksDB, newTask, result).then(function (res) {
+            result(res);
+          });
+        }
+      }
+    });
+  }
+  // TODO: Implement U and D in milestone 3 
+  /*delete a task from the task db*/
+  static deleteTask(tasksDB, taskId, result) {
+    var resultObj;
+    tasksDB.find({
+      _id: new ObjectId(taskId)
+    }).toArray(function (err) {
+      if (err) {
+        resultObj = ResultObj("Error when attempting to delete task!", err);
+        console.log(resultObj.statusMsg + ": " + err);
+        resolve(resultObj);
+      } else {
+        tasksDB.deleteOne({
+          _id: new ObjectId(taskId)
+        }).toArray(function (err2) {
+          if (err2) {
+            resultObj = ResultObj("Error when attempting to delete task!", err);
+            console.log(resultObj.statusMsg + ": " + err);
+            resolve(resultObj);
+          } else {
+            resultObj = ResultObj("user with" + taskId.name + "deleted");
+            resultObj(resultObj);
+          }
+        });
+      }
+    });
+  }
+}
 
 /*
   ResultObj constructor function. Since we need to create a different return object for many different possible scenarios, all this functionality
@@ -24,118 +172,6 @@ function ResultObj(statusMsg = "", statusObj = null, success = false, id = null,
   return returnObj;
 }
 
-/*
-  add a task to the task database
-  if the task can't be added then just return an err message
-*/
-Task.addTask = function (tasksDB, taskName, result) {
-  var resultObj;
-  tasksDB.insertOne(taskName, function (err, res) {
-    if (err) { //Unkown error, return to client and display it in the log.
-      resultObj = ResultObj("Error when adding new Task to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else {
-      resultObj = ResultObj("Added task " + taskName.title, null, true, taskName._id, taskName);
-      result(resultObj);
-    }
-  });
-}
-
-/*
-  get a single task 
-  if the task is not available then the return an err message,else return the task informationto
-*/
-Task.getTask = function (tasksDB, taskId, result) {
-  var resultObj, id;
-  if (taskId == "default") { // If the id is 'default' then we cannot create an ObjectId with it and mus tjust pass it as a string.
-    id = taskId;
-  } else {
-    id = new ObjectId(taskId);
-  }
-  tasksDB.find({
-    _id: id
-  }).toArray(function (err, res) {
-    if (err) {
-      resultObj = ResultObj("Error when adding task to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else if (res.length == 1) {
-      resultObj = ResultObj("Task retrieved", null, true, res[0]._id, res[0]);
-      result(resultObj);
-    } else {
-      resultObj = ResultObj("Task not found");
-      result(resultObj);
-    }
-  });
-}
-
-//get all tasks for a specific user
-Task.getTasks = function (tasksDB, userId, result) {
-  var resultObj;
-  tasksDB.find({
-    owner: userId
-  }).toArray(function (err, res) {
-    if (err) {
-      resultObj = ResultObj("Error when adding task to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else {
-      resultObj = ResultObj("Task retrieved", null, true, userId, res);
-      result(resultObj);
-    }
-  });
-}
-
-// TODO: Implement U and D in milestone 3
-/*update the task for several cases
--name
--category
--priority
--subtask
--timeblock?*/
-Task.updateTask = function (tasksDB, newTask, result) {
-  var resultObj;
-  tasksDB.find({
-    _id: new ObjectId(newTask._id)
-  }).toArray(function (err, res) {
-    if (err) { //Unkown error, return to client and display it in the log.
-      resultObj = ResultObj("Error when checking if user with id " + taskId + " exists in database.", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else if (res.length == 0) { //no user with id userId, tell the updater and log it
-      resultObj = ResultObj("Task not in database. ID:" + taskId._Id);
-      console.log(resultObj.statusMsg);
-      result(resultObj);
-    } else {
-      if (newTask.name != null) {
-        updateTaskName(tasksDB, newTask, result).then(function (res) {
-          result(res);
-        })
-      }
-      if (newTask.category != null) {
-        updateTaskCategory(tasksDB, newTask, result).then(function (res) {
-          result(res);
-        })
-      }
-      if (newTask.priority != null) {
-        updateTaskPriority(tasksDB, newTask, result).then(function (res) {
-          result(res);
-        })
-      }
-      if (newTask.timeBlocks.length > 0) {
-        updateTaskTB(tasksDB, newTask, result).then(function (res) {
-          result(res);
-        })
-      }
-      if (newTask.subTasks.length > 0) {
-        updatesubTask(tasksDB, newTask, result).then(function (res) {
-          result(res);
-        })
-      }
-    }
-  })
-}
 //update the task name
 async function updateTaskName(tasksDB, newTask, result) {
   return new Promise(function (resolve) {
@@ -251,37 +287,6 @@ async function updatesubTask(tasksDB, newTask, result) {
     })
   })
 
-}
-
-
-
-
-// TODO: Implement U and D in milestone 3 
-/*delete a task from the task db*/
-Task.deleteTask = function (tasksDB, taskId, result) {
-  var resultObj;
-  tasksDB.find({
-    _id: new ObjectId(taskId)
-  }).toArray(function (err) {
-    if (err) {
-      resultObj = ResultObj("Error when attempting to delete task!", err);
-      console.log(resultObj.statusMsg + ": " + err);
-      resolve(resultObj);
-    } else {
-      tasksDB.deleteOne({
-        _id: new ObjectId(taskId)
-      }).toArray(function (err2) {
-        if (err2) {
-          resultObj = ResultObj("Error when attempting to delete task!", err);
-          console.log(resultObj.statusMsg + ": " + err);
-          resolve(resultObj);
-        } else {
-          resultObj = ResultObj("user with" + taskId.name + "deleted");
-          resultObj(resultObj);
-        }
-      })
-    }
-  })
 }
 
 module.exports = Task;
--- RESTful API/models/UserModel.js
+++ RESTful API/models/UserModel.js
@@ -5,7 +5,114 @@
 'use strict';
 //var noSql = require('./noSqlDb.js');
 var ObjectId = require('mongodb').ObjectId;
-var User = require('../../Constructors.js').User;
+// var User = require('./SubTask.js/index.js').User;
+class User {
+  constructor(user) {
+    this.name = user.name == null ? null : user.name;
+    this._id = user._id == null ? null : user._id;
+    this.savedTasks = user.savedTasks == null ? [] : user.savedTasks;
+  }
+  // Takes a new User object that contains all data that we want to add.
+  static addUser(usersDB, newUser, result) {
+    var resultObj;
+    usersDB.insertOne(newUser, function (err, res) {
+      if (err) { //Unkown error, return to client and display it in the log.
+        resultObj = ResultObj("Error when adding user to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else { //user was added to the database.
+        resultObj = ResultObj("Added user " + newUser.name, null, true, newUser._id, newUser);
+        result(resultObj);
+      }
+    });
+  }
+  /*
+  GetUser returns the data of the user with the given ID.
+  If no user is found, there is no data retuned and a statusMsg with the reason why there was an error.
+  */
+  static getUser(usersDB, userId, result) {
+    var resultObj;
+    usersDB.find({
+      _id: new ObjectId(userId)
+    }).toArray(function (err, res) {
+      if (err) {
+        resultObj = ResultObj("Error when adding user to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else if (res.length == 1) {
+        resultObj = ResultObj("User retrieved", null, true, res[0]._id, res[0]);
+        result(resultObj);
+      } else {
+        resultObj = ResultObj("user not found");
+        result(resultObj);
+      }
+    });
+  }
+  /*
+  updateUser function is responsible for updating anything that may be stored in a user object.
+  As of milestone 2, the user object is only responsible for storing:
+    * Saved tasks
+    * Name
+    * _id
+  */
+  static updateUser(usersDB, newUser, result) {
+    var resultObj;
+    usersDB.find({
+      _id: new ObjectId(newUser._id)
+    }).toArray(function (err, res) {
+      if (err) { //Unkown error, return to client and display it in the log.
+        resultObj = ResultObj("Error when checking if user with id " + newUser._Id + " exists in database.", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else if (res.length == 0) { //no user with id userId, tell the updater and log it
+        resultObj = ResultObj("User not in database. ID:" + newUser._Id);
+        console.log(resultObj.statusMsg);
+        result(resultObj);
+      } else { //user is in the database!
+        //Three different Cases:
+        //  User adds a new task to save
+        //  User updates their name
+        //  User updates thier other information (Not implemented yet)
+        if (newUser.name != null)
+          updateName(usersDB, newUser, resultObj).then(function (res1) {
+            result(res1);
+          });
+        if (newUser.savedTasks.length > 0)
+          updateSavedTasks(usersDB, newUser, resultObj).then(function (res1) {
+            result(res1);
+          });
+      }
+    });
+  }
+  /*delete a user from the database
+  searching for the userId(for now we will find by userId) and remove them from the database
+  */
+  static deleteUser(usersDB, userId, result) {
+    var resultObj;
+    usersDB.find({
+      _id: new ObjectId(userId)
+    }).toArray(function (err, res) {
+      if (err) {
+        resultObj = ResultObj("Error when deleting user to database", err);
+        console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
+        result(resultObj);
+      } else {
+        usersDB.deleteOne({
+          _id: new ObjectId(userId)
+        }, function (err2) {
+          if (err) {
+            resultObj = ResultObj("Error when user user to database", err2);
+            console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
+            result(resultObj);
+          } else {
+            resultObj = ResultObj("user with ID:" + userId + "was deleted", null, true, null, null);
+            result(resultObj);
+          }
+        });
+      }
+    });
+  }
+}
 
 /*
   ResultObj constructor function. Since we need to create a different return object for many different possible scenarios, all this functionality
@@ -22,83 +129,6 @@ function ResultObj(statusMsg = "", statusObj = null, success = false, id = null,
     data: data,
   };
   return returnObj;
-}
-
-// Takes a new User object that contains all data that we want to add.
-User.addUser = function (usersDB, newUser, result) {
-  var resultObj;
-  usersDB.insertOne(newUser, function (err, res) {
-    if (err) { //Unkown error, return to client and display it in the log.
-      resultObj = ResultObj("Error when adding user to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else { //user was added to the database.
-      resultObj = ResultObj("Added user " + newUser.name, null, true, newUser._id, newUser);
-      result(resultObj);
-    }
-  });
-}
-
-/*
-GetUser returns the data of the user with the given ID.
-If no user is found, there is no data retuned and a statusMsg with the reason why there was an error.
-*/
-User.getUser = function (usersDB, userId, result) {
-  var resultObj;
-  usersDB.find({
-    _id: new ObjectId(userId)
-  }).toArray(function (err, res) {
-    if (err) {
-      resultObj = ResultObj("Error when adding user to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else if (res.length == 1) {
-      resultObj = ResultObj("User retrieved", null, true, res[0]._id, res[0]);
-      result(resultObj);
-    } else {
-      resultObj = ResultObj("user not found");
-      result(resultObj);
-    }
-  });
-}
-
-/* 
-updateUser function is responsible for updating anything that may be stored in a user object.
-As of milestone 2, the user object is only responsible for storing:
-  * Saved tasks
-  * Name
-  * _id
-*/
-User.updateUser = function (usersDB, newUser, result) {
-  var resultObj;
-  usersDB.find({ //Check if the user is in the database
-    _id: new ObjectId(newUser._id)
-  }).toArray(function (err, res) {
-    if (err) { //Unkown error, return to client and display it in the log.
-      resultObj = ResultObj("Error when checking if user with id " + newUser._Id + " exists in database.", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else if (res.length == 0) { //no user with id userId, tell the updater and log it
-      resultObj = ResultObj("User not in database. ID:" + newUser._Id);
-      console.log(resultObj.statusMsg);
-      result(resultObj);
-    } else { //user is in the database!
-
-      //Three different Cases:
-      //  User adds a new task to save
-      //  User updates their name
-      //  User updates thier other information (Not implemented yet)
-
-      if (newUser.name != null)
-        updateName(usersDB, newUser, resultObj).then(function (res1) {
-          result(res1);
-        });
-      if (newUser.savedTasks.length > 0)
-        updateSavedTasks(usersDB, newUser, resultObj).then(function (res1) {
-          result(res1);
-        });
-    }
-  });
 }
 
 async function updateName(usersDB, newUser, resultObj) {
@@ -165,34 +195,6 @@ async function updateSavedTasks(usersDB, newUser, resultObj) {
 }
 
 
-/*delete a user from the database
-searching for the userId(for now we will find by userId) and remove them from the database
-*/
-User.deleteUser = function (usersDB, userId, result) {
-  var resultObj;
-  usersDB.find({
-    _id: new ObjectId(userId)
-  }).toArray(function (err, res) {
-    if (err) {
-      resultObj = ResultObj("Error when deleting user to database", err);
-      console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
-      result(resultObj);
-    } else {
-      usersDB.deleteOne({
-        _id: new ObjectId(userId)
-      }, function (err2) {
-        if (err) {
-          resultObj = ResultObj("Error when user user to database", err2);
-          console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
-          result(resultObj);
-        } else {
-          resultObj = ResultObj("user with ID:" + userId + "was deleted", null, true, null, null);
-          result(resultObj);
-        }
-      });
-    }
-  });
-}
 
 module.exports = {
   User,

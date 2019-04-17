df7998e443b5dee0112ec9d5b7a7381a8a13ae2b
Isaac <isaac.schultz@wsu.edu>
2019-04-17 13:07:14 -0700

    Added dirName to sendFile

--- app/appController.js
+++ app/appController.js
@@ -5,21 +5,21 @@
 'use strict'
 
 exports.getAchievements = function (req, res) {
-  res.sendFile('private/html/achievements.html');
+  res.sendFile(__dirname, 'private/html/achievements.html');
 };
 
 exports.getProgress = function (req, res) {
-  res.sendFile('private/html/progress.html');
+  res.sendFile(__dirname, 'private/html/progress.html');
 };
 
 exports.getSchedule = function (req, res) {
-  res.sendFile('private/html/schedule.html');
+  res.sendFile(__dirname, 'private/html/schedule.html');
 };
 
 exports.getTasklist = function (req, res) {
-  res.sendFile('private/html/tasklist.html');
+  res.sendFile(__dirname, 'private/html/tasklist.html');
 };
 
 exports.getTaskview = function (req, res) {
-  res.sendFile('private/html/taskview.html');
+  res.sendFile(__dirname, 'private/html/taskview.html');
 };

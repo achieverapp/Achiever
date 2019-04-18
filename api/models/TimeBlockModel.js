dbaa13617b547e9018cd81c3836a88fe4f0e45f8
Isaac <isaac.schultz@wsu.edu>
2019-04-17 22:15:01 -0700

    Fully implement CRUD for timeblocks

--- api/models/TimeBlockModel.js
+++ api/models/TimeBlockModel.js
@@ -6,6 +6,7 @@
 
 var ObjectId = require('mongodb').ObjectId;
 
+// Task + startDate combinations will always be unique
 class TimeBlock {
     constructor(timeBlocks) {
         this.task = timeBlocks.task == null ? null : timeBlocks.task;
@@ -29,17 +30,17 @@ class TimeBlock {
             }
         });
     }
+
     /*
-    GettimeBlock returns the data of the timeBlock with the given ID.
+    GettimeBlock returns the data of the timeBlock with the given task and startDate
     If no timeBlock is found, there is no data retuned and a statusMsg with the reason why there was an error.
     */
     static getTimeBlock(timeBlockDB, querytimeBlock, result) {
-        var query = querytimeBlock;
-        if (querytimeBlock._id) {
-            query._id = new ObjectId(querytimeBlock._id)
-        }
         var resultObj;
-        timeBlockDB.find(query).toArray(function (err, res) {
+        timeBlockDB.find({
+            task: querytimeBlock.task,
+            startDate: querytimeBlock.startDate
+        }).toArray(function (err, res) {
             if (err) {
                 resultObj = ResultObj("Error when adding timeBlock to database", err);
                 console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
@@ -55,50 +56,42 @@ class TimeBlock {
     }
 
     //get all time blocks for a specific user
-    static getTimeBlocks(timeBlockDB, userId, result) {
+    static getTimeBlocks(timeBlockDB, querytimeBlock, result) {
         var resultObj;
         timeBlockDB.find({
-            owner: userId
+            day: querytimeBlock.day,
+            owner: querytimeBlock.owner
         }).toArray(function (err, res) {
             if (err) {
                 resultObj = ResultObj("Error when searching for timeblocks for user " + userId, err);
                 console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
                 result(resultObj);
             } else {
-                resultObj = ResultObj("timeblocks retrieved", null, true, userId, res);
+                resultObj = ResultObj("timeblocks retrieved", null, true, querytimeBlock.day, res);
                 result(resultObj);
             }
         });
     }
+
     /*
     updatetimeBlock function is responsible for updating anything that may be stored in a timeBlock object.
-    As of milestone 2, the timeBlock object is only responsible for storing:
-      * Saved tasks
-      * Name
-      * _id
     */
     static updateTimeBlock(timeBlockDB, newtimeBlock, result) {
         var resultObj;
         timeBlockDB.find({
-            _id: new ObjectId(newtimeBlock._id)
+            task: newtimeBlock.task,
+            startDate: newtimeBlock.startDate
         }).toArray(function (err, res) {
             if (err) { //Unkown error, return to client and display it in the log.
-                resultObj = ResultObj("Error when checking if timeBlock with id " + newtimeBlock._Id + " exists in database.", err);
+                resultObj = ResultObj("Error when checking if timeBlock is available.", err);
                 console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
                 result(resultObj);
             } else if (res.length == 0) { //no timeBlock with id timeBlockId, tell the updater and log it
-                resultObj = ResultObj("timeBlock not in database. ID:" + newtimeBlock._Id);
+                resultObj = ResultObj("timeBlock is not available");
                 console.log(resultObj.statusMsg);
                 result(resultObj);
-            } else { //timeBlock is in the database!
-                //Three different Cases:
-                //  timeBlock adds a new task to save
-                //  timeBlock updates their name
-                //  timeBlock updates thier other information (Not implemented yet)
-                if (newtimeBlock.name != null)
-                    updateName(timeBlockDB, newtimeBlock, resultObj).then(result); //change these to things we need later
-                if (newtimeBlock.savedTasks.length > 0)
-                    updateSavedTasks(timeBlockDB, newtimeBlock, resultObj).then(result);
+            } else { //timeBlock is in the database!          
+                updateDate(timeBlockDB, newtimeBlock, resultObj).then(result); //change these to things we need later                
             }
         });
     }
@@ -106,25 +99,27 @@ class TimeBlock {
     /*delete a timeBlock from the database
     searching for the timeBlockId(for now we will find by timeBlockId) and remove them from the database
     */
-    static deleteTimeBlock(timeBlockDB, timeBlockId, result) {
+    static deleteTimeBlock(timeBlockDB, querytimeBlock, result) {
         var resultObj;
         timeBlockDB.find({
-            _id: new ObjectId(timeBlockId)
+            task: querytimeBlock.task,
+            startDate: querytimeBlock.startDate
         }).toArray(function (err, res) {
             if (err) {
-                resultObj = ResultObj("Error when deleting timeBlock to database", err);
+                resultObj = ResultObj("Error when deleting timeBlock", err);
                 console.log(resultObj.statusMsg + ": " + JSON.stringify(err));
                 result(resultObj);
             } else {
                 timeBlockDB.deleteOne({
-                    _id: new ObjectId(timeBlockId)
+                    task: querytimeBlock.task,
+                    startDate: querytimeBlock.startDate
                 }, function (err2) {
-                    if (err) {
-                        resultObj = ResultObj("Error when timeBlock timeBlock to database", err2);
+                    if (err2) {
+                        resultObj = ResultObj("Error when deleting timeblock", err2);
                         console.log(resultObj.statusMsg + ": " + JSON.stringify(err2));
                         result(resultObj);
                     } else {
-                        resultObj = ResultObj("timeBlock with ID:" + timeBlockId + "was deleted", null, true, null, null);
+                        resultObj = ResultObj("timeBlock was deleted", null, true, null, null);
                         result(resultObj);
                     }
                 });
@@ -156,68 +151,31 @@ function ResultObj(statusMsg = "", statusObj = null, success = false, id = null,
     return returnObj;
 }
 
-async function updateName(timeBlockDB, newtimeBlock, resultObj) {
+async function updateDate(timeBlockDB, newtimeBlock, resultObj) {
     return new Promise(function (resolve) {
         timeBlockDB.updateOne({ //find the timeBlock to update
-                _id: new ObjectId(newtimeBlock._id)
+                task: newtimeBlock.task,
+                startDate: newtimeBlock.startDate
             }, { //update its data with:
                 $set: {
-                    'name': newtimeBlock.name
+                    'startDate': newtimeBlock.startDate,
+                    'endDate': newtimeBlock.endDate
                 }
             },
             function (err) {
                 if (err) { //Unkown error, return to client and display it in the log.
-                    resultObj = ResultObj("Error when attempting to change name!", err);
+                    resultObj = ResultObj("Error when attempting to update timeblock!", err);
                     console.log(resultObj.statusMsg + ": " + err);
                     resolve(resultObj);
                 } else { //hole updated successfully!
-                    resultObj = ResultObj("Name channged to " + newtimeBlock.name, null, true);
+                    resultObj = ResultObj("start date changed to " + newtimeBlock.startDate, null, true, null, newtimeBlock);
                     resolve(resultObj);
                 }
             });
     });
 }
 
-async function updateSavedTasks(timeBlockDB, newtimeBlock, resultObj) {
-    return new Promise(function (resolve) {
-        timeBlockDB.find({
-                _id: new ObjectId(newtimeBlock._id),
-                savedTasks: {
-                    $elemMatch: { //mongoDB to match anything that also matches the data inside the property
-                        taskId: newtimeBlock.savedTasks.taskId
-                    }
-                }
-            })
-            .toArray(function (err, res) {
-                if (err) { //Unkown error, return to client and display it in the log.
-                    resultObj = ResultObj("Error when locating timeBlock", err);
-                    console.log(resultObj.statusMsg + ": " + err);
-                    resolve(statusObj);
-                }
-                if (res.length == 0) { //Insert a task ID if it is not already in the data
-                    timeBlockDB.updateOne({ //select the timeBlock with the given timeBlockId to update
-                            _id: new ObjectId(newtimeBlock._id)
-                        }, {
-                            $push: { //adds the holedata as a new element of the holes array
-                                savedTasks: {
-                                    $each: newtimeBlock.savedTasks
-                                }
-                            },
-                        },
-                        function (err2) {
-                            if (err2) { //Unkown error, return to client and display it in the log.
-                                resultObj = ResultObj("Error when attempting to save task ID: " + newtimeBlock.savedTasks + " for timeBlock " + newtimeBlock.timeBlockName, err2);
-                                console.log(resultObj.statusMsg + ": " + err2);
-                                resolve(resultObj);
-                            } else { //Task was added successfully!
-                                resultObj = ResultObj("Task saved as template for timeBlock " + newtimeBlock.timeBlockName, null, true);
-                                resolve(resultObj);
-                            }
-                        });
-                }
-            });
-    });
-}
+
 
 
 

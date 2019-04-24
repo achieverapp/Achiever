043707be7eb6160b8e64f2a6afca0dab749b6d4b
cnguyen <chris.m.nguyen@wsu.edu>
2019-04-23 23:05:49 -0700

    commit for 4/23/18

--- app/public/js/achievements.js
+++ app/public/js/achievements.js
@@ -10,13 +10,48 @@ $(document).ready(function()
 
     
 })
-
+//build the list of user achievements
 function buildAchievementsList()
 {
-getUserAchievements({
-    owner:currUserId
-},function(result,error)
+    //not yet in the html file will add in
+    var achievementsUL=document.getElementById("achievements-list");
+    var userAchievements;
+    getUserAchievements(
+    {
+        owner:currUserId
+    },function(result,error)
+    {   
+    userAchievements=result.data;
+    var completed=[];
+
+    userAchievements.forEach(userAchievement => 
+            {userAchievements.achievementId
+            completed.push(userAchievement);
+            achievementsUL.appendChild(buildAchievementCard(userAchievement));
+            });
+    })
+}
+function buildAchievementsCard(userAchievement)
 {
-    
-})
+var achievementCardNode=document.createElement("li");
+achievementCardNode.id=userAchievement.achievementId;
+achievementCardNode.addClass("card");
+achievementCardNode.addClass("achievement-card")
+achievementCardNode.innerHTML=
+"<div class=achievement-card-container"+
+"<h2 class='fas fa-trophy' style=display: 'inline' border-color: 'white'>&nbsp;</h2>"+
+"<div class= card-header>"+
+"<h2 class='achievements-title'>"+userAchievement.achievementId.title+"</h2>"+
+"</div>"+
+"<div class=container>"+
+"<p class='achievements-description'>&nbsp"+userAchievement.achievementId.description+"</p>"+
+"<h2 class"
+
+
+
+
+
+
+
+return achievementCardNode;
 }

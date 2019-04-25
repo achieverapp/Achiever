56a4c4ecbcfecdafae7e6ba99419b9609e576407
Isaac <Isaac.Schultz@wsu.edu>
2019-04-24 22:10:45 -0700

    Added recently completed tasks

---
+++ app/public/js/progress.js
@@ -0,0 +1,146 @@
+/*
+    tasklist.js
+    This file is responsible for dealing with dynamic javascript functionality for the tasklist page
+    It loads the tasks for the current user and allows them to create and modify tasks.
+*/
+
+/**
+ * Maps priority numbers to the css styles for them
+ */
+var priorityToClassMap = {
+    0: "priority-low",
+    1: "priority-med",
+    2: "priority-high",
+    "0": "priority-low",
+    "1": "priority-med",
+    "2": "priority-high",
+};
+
+/**
+ * Initializes the page, and all event handlers when the page finishes loading
+ */
+$(document).ready(function () {
+    // load the navbar
+    $("#navbar").load("/html/navbar.html", onNavBarLoad);
+
+    buildTaskList("sortByDueDate"); //Build the task list, sorting by due date.
+
+    //load event handler for changing sort method
+    $(".sortby-dropdown-item").click(changeSortMethod);
+
+    // Event handler for when a task is clicked on. Should bring the user to the task view page.
+    // Adds a new empty row for the task to allow the user to input another task
+    $(document).on("click", ".task-card", taskCardClicked);
+});
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////// Event Handlers ///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Event handler for when a task card is clicked. Brings the user to the taskview page for the task they clicked on.
+ * @param {*} e: click event object
+ */
+function taskCardClicked(e) {
+    var userId = getQueryParam('userId')
+    if ($(e.target).hasClass("task-card-container"))
+        window.location.href = `/taskview?taskId=${e.target.parentElement.id}&userId=${userId}`;
+}
+
+/**Load event handler for navbar HTML being added to the page */
+function onNavBarLoad() {
+    $("#nav-tasklist").addClass("nav-active");
+}
+
+/** Click event handler for the new task button.
+ * Opens the taskview page for a new task, with taskId of 'default'
+ */
+function loadTaskView() {
+    var userId = getQueryParam("userId")
+    window.location.href = `/taskview?taskId=default&userId=${userId}`;
+}
+
+/**
+ * Event handler for when the sort method is changed.
+ * @param {*} e: click event arguments
+ */
+function changeSortMethod(e) {
+    $(".sortby-dropdown").each(function () { //set the title of the button to the dropdown that was selected.
+        var element = $(this);
+        element.html(e.target.innerText);
+    });
+
+    $(".sortby-dropdown").data("sortBy", e.target.id);
+    buildTaskList(e.target.id);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////// General Use functions ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Builds the list of tasks for the page with a given sort function.
+ * Puts them into two categories, overdue tasks and upcoming tasks.
+ * @param sortBy: sorting function to compare each task.
+ */
+function buildTaskList(sortBy) {
+    var completedUL = document.getElementById("completed-list");
+    var tasks;
+    getTaskList(function (result, error) {
+        tasks = result.data
+        if (sortBy === "sortByPriority") {
+            tasks.sort(compareTaskByPriorityDescending);
+        } else if (sortBy === "sortByDueDate") {
+            tasks.sort(compareTaskByDateDescending);
+        }
+        
+        tasks = getCheckedTasks(tasks)
+
+        while (completedUL.firstChild) {
+            completedUL.removeChild(completedUL.firstChild);
+        }
+
+        tasks.forEach(task => {
+            if (task.completed == null && task.checked) {             
+                completedUL.appendChild(buildTaskCard(task));                
+            }
+        });
+    });
+}
+
+/**
+ * Adds a task card on to the page
+ * @param {*} task: Task object to add to the page
+ */
+function buildTaskCard(task) {
+    var dueDate = new Date(task.due);
+    var taskCardNode = document.createElement("li");
+    taskCardNode.id = task._id;
+    taskCardNode.classList.add("card");
+    taskCardNode.classList.add("task-card");
+    taskCardNode.classList.add(priorityToClassMap[task.priority]);
+    taskCardNode.innerHTML =
+        "<div class='task-card-container'>" +
+        "<h2 class='fas fa-check-square align-middle task-checkbox task-incomplete'></h2>" +
+        "<div class='align-middle task-card-content'>" +
+        "<h3 class='task-card-title' style='display:block'>" + task.title + "</h3>" +
+        "<p class='task-due'>Due:&nbsp;" + formatDateTime(dueDate) + "</p>" +
+        "<p class='task-category'>Category:&nbsp;" + task.category + "</p>" +
+        "</div>" +
+        "</div>";
+    return taskCardNode;
+}
+
+/** Turns a Javascript Date object into local time */
+function formatDateTime(date) {
+    return date.toLocaleString();
+}
+
+/**
+ * Retrieved a quer parameter
+ * @param {*} param: name of the query parameter that is to e retrieved from the URL
+ */
+function getQueryParam(param) {
+    var url = window.location.href;
+    return url.split(`${param}=`)[1].split('&')[0]
+}
